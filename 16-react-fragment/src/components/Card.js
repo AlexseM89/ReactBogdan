884eb6826af7f21d80a91006318b4011560cb776
Aleksei Melenchuk <melenchuk89@mail.ru>
2024-06-20 17:44:38 +0300

    React Fragment

---
+++ 16-react-fragment/src/components/Card.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Card = () => {
+    return (
+        <React.Fragment>
+            <h1>Aleksei</h1>
+            <h3>I am reding React course</h3>
+            <button>Like!</button>
+        </React.Fragment>
+    );
+};
+export default Card;
